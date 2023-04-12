2ebc272fa87703caf611306d4355f147e7cf3c9c
feliciteuwase <felciteuwase98@gmail.com>
2023-04-11 21:42:54 -0400

    a9 final

---
+++ controllers/tuits/tuits-schema.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+const schema = mongoose.Schema({
+  tuit: String,
+  likes: Number,
+  liked: Boolean,
+  topic: String,
+  userName: String,
+  handle: String,
+  time: String,
+  image: String,
+  title: String,
+  dislikes: Number,
+  disliked: Boolean,
+  replies: Number,
+  retuits: Number,
+}, {collection: 'tuits'});
+export default schema;
+
